d8b121af42647ec6242fb321bbed0490f43d661a
Isas321 <javier.isas321@gmail.com>
2024-10-31 20:23:02 -0300

    post productos

--- MiWebAPI/Controllers/ProductosController.cs
+++ MiWebAPI/Controllers/ProductosController.cs
@@ -13,7 +13,16 @@ public class ProductosController : ControllerBase
     [HttpGet("GetProductos", Name = "GetProductos")]
     public IEnumerable<Producto> GetProductos()
     {
-      ProductosRepositorio prodRepo = new ProductosRepositorio();
-      return prodRepo.GetAll();
+      ProductosRepositorio produstosRepositorio = new ProductosRepositorio();
+      return produstosRepositorio.GetAll();
     }
+
+  [HttpPost("PostProductos", Name = "PostProductos")]
+  public IActionResult PostProductos(string descripcion, int precio)
+  {
+      ProductosRepositorio productosRepositorio = new ProductosRepositorio();
+      var producto = new Producto(descripcion, precio);
+      productosRepositorio.Create(producto);
+      return Ok("Producto creado correctamente");
+  }
 }
