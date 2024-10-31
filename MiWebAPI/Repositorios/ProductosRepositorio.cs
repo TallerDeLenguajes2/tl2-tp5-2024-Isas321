6019f3bf774651dec78b2be7c0690f10ac0bada4
Isas321 <javier.isas321@gmail.com>
2024-10-31 20:22:28 -0300

    create con solo descripcion y precio

--- MiWebAPI/Repositorios/ProductosRepositorio.cs
+++ MiWebAPI/Repositorios/ProductosRepositorio.cs
@@ -9,28 +9,40 @@ namespace MiWebAPI.Repositorios
 {
   public class ProductosRepositorio : IProductosRepositorio
   {
-    public void Create(Producto producto){
-
+      public void Create(Producto producto){
+        var cadena = "Data Source = db/Tienda.db";
+        using( var sqlitecon = new SqliteConnection(cadena)){
+          // Importante crear y destruir!
+          sqlitecon.Open();
+          var consulta = @"INSERT INTO Productos (Descripcion, Precio) VALUES (@Descripcion, @Precio);";
+          SqliteCommand comand = new SqliteCommand(consulta, sqlitecon);
+          // Agregar parámetros para evitar inyecciones SQL
+          comand.Parameters.AddWithValue("@Descripcion", producto.Descripcion);
+          comand.Parameters.AddWithValue("@Precio", producto.Precio);
+          // Ejecutar el comando
+          comand.ExecuteNonQuery();
+          sqlitecon.Close(); //Me aseguro que la BD queda liberada
+        }
     }
     public List<Producto> GetAll()
     {
       List<Producto> productos = new List<Producto>();
       var cadena = "Data Source = db/Tienda.db";
       using( var sqlitecon = new SqliteConnection(cadena)){
-      // Importante crear y destruir!
-      sqlitecon.Open();
-      var consulta = @"SELECT * FROM Productos;";
-      SqliteCommand comand = new SqliteCommand(consulta, sqlitecon);
-      var reader = comand.ExecuteReader();
-      while(reader.Read())
-      {
-          var Id = Convert.ToInt32(reader["idProducto"]);
-          var Nombre = reader["Descripcion"].ToString();
-          var Precio = Convert.ToInt32(reader["Precio"]);
-          var producto = new Producto(Id, Nombre, Precio);
-          productos.Add(producto);
-      }
-      sqlitecon.Close(); //Me aseguro que la BD queda liberada
+        // Importante crear y destruir!
+        sqlitecon.Open();
+        var consulta = @"SELECT * FROM Productos;";
+        SqliteCommand comand = new SqliteCommand(consulta, sqlitecon);
+        var reader = comand.ExecuteReader();
+        while(reader.Read())
+        {
+            var Id = Convert.ToInt32(reader["idProducto"]);
+            var Nombre = reader["Descripcion"].ToString();
+            var Precio = Convert.ToInt32(reader["Precio"]);
+            var producto = new Producto(Id, Nombre, Precio);
+            productos.Add(producto);
+        }
+        sqlitecon.Close(); //Me aseguro que la BD queda liberada
       }
       return productos;
     }
@@ -51,7 +63,7 @@ namespace MiWebAPI.Repositorios
           Nombre = reader["Descripcion"].ToString();
           Precio = Convert.ToInt32(reader["Precio"]);
       } 
-      var producto = new Producto(Id, Nombre, Precio);
+      var producto = new Producto(Nombre, Precio);
       sqlitecon.Close(); //Me aseguro que la BD queda liberada
       return (producto);
       }
