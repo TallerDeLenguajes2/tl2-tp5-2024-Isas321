66b5cd826bf8efd8d4fd8c4d2fbe0c3048789dc1
Javier Alfredo Isasmendi <javier.isas321@gmail.com>
2024-11-01 12:03:40 -0300

    obtener productos por id y borrado de producto

--- MiWebAPI/Controllers/ProductosController.cs
+++ MiWebAPI/Controllers/ProductosController.cs
@@ -6,23 +6,52 @@ using MiWebAPI.Repositorios;
 namespace MiWebAPI.Controllers;
 
 [ApiController]
-[Route("[controller]")]
+[Route("api/[controller]")]
 
 public class ProductosController : ControllerBase
 {
-    [HttpGet("GetProductos", Name = "GetProductos")]
-    public IEnumerable<Producto> GetProductos()
-    {
-      ProductosRepositorio produstosRepositorio = new ProductosRepositorio();
-      return produstosRepositorio.GetAll();
-    }
-
   [HttpPost("PostProductos", Name = "PostProductos")]
-  public IActionResult PostProductos(string descripcion, int precio)
+  public ActionResult PostProductos(string descripcion, int precio)
   {
       ProductosRepositorio productosRepositorio = new ProductosRepositorio();
       var producto = new Producto(descripcion, precio);
       productosRepositorio.Create(producto);
       return Ok("Producto creado correctamente");
+  }
+
+
+  [HttpGet("GetProductos", Name = "GetProductos")]
+  public IEnumerable<Producto> GetProductos()
+  {
+    ProductosRepositorio produstosRepositorio = new ProductosRepositorio();
+    return produstosRepositorio.GetAll();
+  }
+
+
+  [HttpGet("GetProductoPorId/{id}", Name = "GetProductoPorId/{id}")]
+  public ActionResult<Producto> GetProductoById(int id)
+  {
+    ProductosRepositorio produstosRepositorio = new ProductosRepositorio();
+    return produstosRepositorio.GetProductoPorId(id);
+  }
+
+
+  // [HttpPut("PutProductos/{id}", Name = "PutProductos/{id}")]
+  // public ActionResult PutProductos(int id)
+  // {
+  //   ProductosRepositorio productoRepositorio = new ProductosRepositorio();
+  //   productoRepositorio.ActualizarNombrePorId(id);
+
+  //   return Ok();
+  // }
+
+  [HttpDelete("DeleteProductos/{id}", Name = "DeleteProductos/{id}")]
+  public ActionResult DeleteProductos(int id)
+  {
+    ProductosRepositorio productoRepositorio = new ProductosRepositorio();
+    if(productoRepositorio.Remove(id))
+      return Ok("Producto eliminado");
+    else
+      return BadRequest("No se pudo eliminar el producto");
   }
 }
