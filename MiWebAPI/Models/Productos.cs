ce2994eced8377a5da0b6c6914e00d601b0a933a
Isas321 <javier.isas321@gmail.com>
2024-11-11 14:51:10 -0300

    corrijo precio a double

--- MiWebAPI/Models/Productos.cs
+++ MiWebAPI/Models/Productos.cs
@@ -1,27 +1,13 @@
 namespace MiWebAPI.Models
 {
-    // public class ProductoCreateModel
-    // {
-    //     public string Descripcion { get; set; }
-    //     public int Precio { get; set; }
-    // }
     public class Producto
     {
         private int idProducto;
         private string descripcion;
-        private int precio;
+        private double precio;
 
-        // Constructor vacío
-        public Producto() { }
 
-        // Constructor sin ID la base de datos lo autoincrementará
-        public Producto(string descripcion, int precio)
-        {
-            this.descripcion = descripcion;
-            this.precio = precio;
-        }
-
-        public Producto(int idProducto, string descripcion, int precio)
+        public Producto(int idProducto, string descripcion, double precio)
         {
             this.idProducto = idProducto;
             this.descripcion = descripcion;
@@ -31,7 +17,7 @@ namespace MiWebAPI.Models
         // Propiedades
         public int IdProducto { get => idProducto; set => idProducto = value; }
         public string Descripcion { get => descripcion; set => descripcion = value; }
-        public int Precio { get => precio; set => precio = value; }
+        public double Precio { get => precio; set => precio = value; }
 
         // Método para mostrar detalles del producto
         public string MostrarProducto()
